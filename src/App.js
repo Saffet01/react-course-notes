c5f9242b66419ae1a0debe7e49a06f8d7a458f2f
Saffet01 <saffetr.kaban@gmail.com>
2023-01-04 01:00:07 +0300

    Intro

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       merhaba dünya
+//     </div>
+//   );
+// }
+
+// fonksiyonu class'a çevirerek de kullanabiliriz bu yapıyı
+class App extends Component{
+  render(){
+
+    const sayi = 26;
+
+    return (
+      <div className="App">
+        merhaba dünya
+        <h1>2+2</h1>
+        <h1>{2+2}</h1>
+        "saffet".toUpperCase() <br></br>
+        {"saffet".toUpperCase()}
+        <h4>{sayi}</h4>
+      </div>
+    );
+  }
 }
 
 export default App;
